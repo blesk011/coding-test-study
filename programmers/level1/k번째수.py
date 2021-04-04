350d5fbce35e0518f89976f9a8dddb620a0366bf
blesk011 <blesk0111@gmail.com>
2021-04-05 00:40:24 +0900

    Add k번째수
    level1

---
+++ programmers/level1/k번째수.py
@@ -0,0 +1,14 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        sub_array = sorted(array[i-1:j])
+        answer.append(sub_array[k-1])
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution([1, 5, 2, 6, 3, 7, 4],
+                   [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
